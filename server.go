bc40b72d595954e82975b237c88a968f10c826f4
VasylKosiv <59533908+VasylKosiv@users.noreply.github.com>
2020-02-24 06:49:55 +0200

    change route

--- server.go
+++ server.go
@@ -1,4 +1,4 @@
-package eventsource
+package main
 
 import (
 	"flag"
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	addr := flag.String("a", ":80", "address of app")
+	addr := flag.String("a", ":8080", "address of app")
 	flag.Parse()
 
 	event := newEventStoreDb()
@@ -17,12 +17,12 @@ func main() {
 
 	mainRoute := mux.NewRouter()
 	apiRoute := mainRoute.PathPrefix("/api/v1").Subrouter()
-	apiRoute.HandleFunc("/list", event.AddEvent).Methods("POST")
-	apiRoute.HandleFunc("/list/{ID}", event.InsertEvent).Methods("PUT")
-	apiRoute.HandleFunc("/list/{id, version}", event.GetEventByVersion).Methods("GET")
-	apiRoute.HandleFunc("/list/{id, timestamp}", event.GetEventByTimestamp).Methods("GET")
-	apiRoute.HandleFunc("/list/{id, versionMin, versionMax, PAGE}", event.GetEventByRangeOfVersion).Methods("GET")
-	apiRoute.HandleFunc("/list/{id, timeMin, timeMax, PAGE}", event.GetEventByRangeOfTimestamp).Methods("GET")
+	apiRoute.HandleFunc("/list", event.InsertEventWithoutID).Methods("POST")
+	apiRoute.HandleFunc("/list/{ID}", event.InsertEvent).Methods("POST")
+	apiRoute.HandleFunc("/list/version/{VERSION}", event.GetEventByVersion).Methods("GET")
+	apiRoute.HandleFunc("/list/{ID}", event.GetEventByRangeOfVersion).Methods("GET")
+	apiRoute.HandleFunc("/list", event.GetEventByTimestamp).Methods("GET")
+	apiRoute.HandleFunc("/list/time/{ID}", event.GetEventByRangeOfTimestamp).Methods("GET")
 
 	if err := http.ListenAndServe(*addr, mainRoute); err != nil {
 		log.Fatal(err.Error())
