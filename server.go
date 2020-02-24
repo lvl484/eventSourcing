1717754cd6eae1c2cd1942330c3126d518363c47
VasylKosiv <59533908+VasylKosiv@users.noreply.github.com>
2020-02-24 16:39:35 +0200

    create new func for mux.Router

--- server.go
+++ server.go
@@ -4,8 +4,6 @@ import (
 	"flag"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -15,16 +13,9 @@ func main() {
 	event := newEventStoreDb()
 	defer event.db.Close()
 
-	mainRoute := mux.NewRouter()
-	apiRoute := mainRoute.PathPrefix("/api/v1").Subrouter()
-	apiRoute.HandleFunc("/list", event.InsertEventWithoutID).Methods("POST")
-	apiRoute.HandleFunc("/list/{ID}", event.InsertEvent).Methods("POST")
-	apiRoute.HandleFunc("/list/version/{VERSION}", event.GetEventByVersion).Methods("GET")
-	apiRoute.HandleFunc("/list/{ID}", event.GetEventByRangeOfVersion).Methods("GET")
-	apiRoute.HandleFunc("/list", event.GetEventByTimestamp).Methods("GET")
-	apiRoute.HandleFunc("/list/time/{ID}", event.GetEventByRangeOfTimestamp).Methods("GET")
+	route := event.NewRoute()
 
-	if err := http.ListenAndServe(*addr, mainRoute); err != nil {
+	if err := http.ListenAndServe(*addr, route); err != nil {
 		log.Fatal(err.Error())
 	}
 }
