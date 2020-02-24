e82c4ee18e76ffb8a148dbc3d69515155eea6e2e
VasylKosiv <59533908+VasylKosiv@users.noreply.github.com>
2020-02-24 06:46:09 +0200

    Change strucy of field data

--- eventstore.go
+++ eventstore.go
@@ -1,14 +1,42 @@
-package eventsource
+package main
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+)
 
 type Eventstore struct {
-	id        string   `json:"ID"`
-	version   int      `json:"Version"`
-	data      struct{} `json:"Data"`
-	timestamp string   `json:"Time"`
+	Version   int    `json:"version"`
+	Id        string `json:"id"`
+	Data      DATA   `json:"data"`
+	Timestamp string `json:"time"`
 }
 
-type data struct {
-	name     string `json:"Name`
-	age      int    `json:"Age`
-	marriage bool   `json:"Status"`
+type DATA map[string]interface{}
+
+//Value method marshall the map to JSONB data (= []byte)
+func (p DATA) Value() (driver.Value, error) {
+	j, err := json.Marshal(p)
+	return j, err
+}
+
+func (p *DATA) Scan(src interface{}) error {
+	source, ok := src.([]byte)
+	if !ok {
+		return errors.New("Type assertion .([]byte) failed.")
+	}
+
+	var i interface{}
+	err := json.Unmarshal(source, &i)
+	if err != nil {
+		return err
+	}
+
+	*p, ok = i.(map[string]interface{})
+	if !ok {
+		return errors.New("Type assertion .(map[string]interface{}) failed.")
+	}
+
+	return nil
 }
